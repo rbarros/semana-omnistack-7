f561f7a751706707841e4ae74319e3204d8fe9e8
Tarcísio Nunes <github@tarcnux.com.br>
2019-06-14 02:09:56 -0300

    Mongo adicionado

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://instarocket:OG9PwoHvDrI7OGlf@cluster0-v8wd5.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+});
 
 //Rota Express
 app.get('/', (req, res) => {

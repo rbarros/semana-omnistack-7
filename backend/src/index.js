b6d8148837ba1f1a1a3f5b7d599d6a5c9a53417f
Tarcísio Nunes <github@tarcnux.com.br>
2019-06-15 22:23:15 -0300

    Acesso às imagens na rota files

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://instarocket:OG9PwoHvDrI7OGlf@cluster0-v8wd5.mongodb.net/test?retryWrites=true&w=majority',{
     useNewUrlParser: true,
 });
+
+//Ao acessar a rota files, redireciona para as imagens redimensionadas
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
